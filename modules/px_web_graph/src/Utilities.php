370acce931ccb8278c84b280a25452d699fcc009
Bartal Clementsen <bartalclementsen@hotmail.com>
2018-04-08 15:15:25 +0100

    Ábyrgdari í PX-meta modulinum: Null'ari má hondterast

--- modules/px_web_graph/src/Utilities.php
+++ modules/px_web_graph/src/Utilities.php
@@ -7,19 +7,18 @@ use mb_detect_encoding;
 
 class Utilities {
     public function getPxFile(string $address) {
-        $pxFileData = "";
+        $px = null;
         try {
+            $pxFileData = "";
             $pxRequestRaw = file_get_contents($address);
             if ($pxRequestRaw) {
                 $encoding = mb_detect_encoding($pxRequestRaw, 'iso-8859-15', true);
                 $pxFileData = mb_convert_encoding($pxRequestRaw, 'UTF-8', $encoding);
+                $px = new Px($pxFileData);
             }
         }
-        catch (Exception $e) {
-            echo $e;
-        }  
+        catch (Exception $e) { }  
 
-        $px = new Px($pxFileData);
         return $px;
     }
 }
